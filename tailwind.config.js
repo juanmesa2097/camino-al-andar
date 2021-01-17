232ef56a6a7e4c1cb94dcde6c348d595e5e1326a
juanmesa2097 <juanmesa2097@gmail.com>
2021-01-17 00:21:12 -0500

    chore: configure tailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   purge: ["./pages/**/*.js", "./components/**/*.js"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      display: ["Bodoni Moda", "serif"],
+      body: ["Montserrat", "sans-serif"],
+    },
     extend: {},
   },
   variants: {
